96523b2eb7314c20cdb2f1b3afbb701d6fa09192
Egor Tkachenko <68852747+HalcyonForest@users.noreply.github.com>
2022-10-15 16:30:35 +0300

    Update utils.py

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,14 @@
 # Don't forget to support cases when target_text == ''
+import editdistance
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
-
+    return editdistance.distance(target_text, predicted_text) / len(target_text)
+    
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    splitted_target = target_text.split(' ')
+    splitted_predict = predicted_text.split(' ')
+    return editdistance.distance(splitted_target, splitted_predict) / len(splitted_target)
+    
+
+    
